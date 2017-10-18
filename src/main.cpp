620266c4bd5ad431e54d956f77df228c6918e693
Nico De Witte <nico.dewitte@outlook.com>
2017-10-18 13:12:26 +0200

    Example program that just detects changes from the io relay card and displays them (using interrupts from wiring Pi)

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string>
+#include <errno.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <bitset>
+#include "lib/drivers/pcf8575.h"
+#include <thread>         // std::thread
+#include <time.h>         /* time */
+#include <vector>
+#include "lib/hal/io_relay_card.h"
+
+#define BUTTON_PIN 1
+
+using namespace std;
+using namespace BiosHomeAutomator;
+
+IORelayCard * relayCard;
+
+void relayCardInputChanged(void) {
+  vector<Input*> inputs = relayCard->get_changed_inputs();
+  for (unsigned int i = 0; i < inputs.size(); i++) {
+    cout << inputs[i]->to_string() << endl;
+  }
+}
+
+int main(void) {
+  if (wiringPiSetup () < 0) {
+			std::cout << "Unable to setup wiringPi" << std::endl;
+      return 1;
+  }
+
+	relayCard = new IORelayCard(0x20, 1);
+
+  if (wiringPiISR (BUTTON_PIN, INT_EDGE_BOTH, &relayCardInputChanged) < 0) {
+			std::cout << "Unable to setup ISR" << std::endl;
+      return 1;
+  }
+
+  while (true) {
+    sleep(1);
+  }
+
+	delete relayCard;
+
+  return 0;
+}
